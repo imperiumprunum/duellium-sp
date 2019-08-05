4abdb0ba1fbf55323b7dcaa382b458d999780312
Imperium Prunum <adamson144144@gmail.com>
2019-08-05 13:43:37 +0200

    Player head towards mouse position, bullets sprites, shooting

---
+++ Assets/Scripts/PLAYER_BULLET.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PLAYER_BULLET : MonoBehaviour {
+
+    float bulletSpeed = 5f;
+
+    private void FixedUpdate()
+    {
+        transform.Translate(Vector3.up * bulletSpeed * Time.deltaTime);
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -11,14 +11,19 @@ public class Player : MonoBehaviour {
     [SerializeField]
     float speed;
 
+    //  BULLET
+    Object bulletPrefab;
+
 	// Use this for initialization
 	void Start () {
-		
+        bulletPrefab = Resources.Load("Prefabs/PLAYER_BULLET");
 	}
 	
 	// Update is called once per frame
 	void Update () {
         Movement();
+        TrackCursor();
+        Shoot();
 	}
 
     void Movement()
@@ -26,7 +31,22 @@ public class Player : MonoBehaviour {
         input = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0);
         direction = input.normalized;
         velocity = direction * speed * Time.deltaTime;
-        transform.Translate(velocity, Space.World);
-        Debug.Log(input);
+       transform.Translate(velocity, Space.World);
+    }
+
+    void TrackCursor()
+    {
+        Vector3 camDir = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        float angle = -(Mathf.Atan2(camDir.x, camDir.y) * Mathf.Rad2Deg);
+        Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, speed * Time.deltaTime);
+    }
+
+    void Shoot()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GameObject bullet = (GameObject)Instantiate(bulletPrefab, transform.position ,transform.rotation);
+        }
     }
 }
