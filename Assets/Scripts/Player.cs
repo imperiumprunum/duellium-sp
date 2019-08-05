7b91feffaaf51face772aa4e442a79c1b3368b26
Imperium Prunum <adamson144144@gmail.com>
2019-08-05 12:54:54 +0200

    Player script, sprites & movement

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    //  MOVEMENT
+    Vector3 input;
+    Vector3 direction;
+    Vector3 velocity;
+    [SerializeField]
+    float speed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Movement();
+	}
+
+    void Movement()
+    {
+        input = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0);
+        direction = input.normalized;
+        velocity = direction * speed * Time.deltaTime;
+        transform.Translate(velocity, Space.World);
+        Debug.Log(input);
+    }
+}
